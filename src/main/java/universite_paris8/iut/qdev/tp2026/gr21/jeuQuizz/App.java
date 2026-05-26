38e2ad0a39c875d297a08179096e9f4488b64d98
Léa <lbaron@iut.univ-paris8.fr>
2026-05-26 16:24:03 +0200

    first commit lea

--- src/main/java/universite_paris8/iut/qdev/tp2026/gr21/jeuQuizz/App.java
+++ src/main/java/universite_paris8/iut/qdev/tp2026/gr21/jeuQuizz/App.java
@@ -5,6 +5,6 @@ package universite_paris8.iut.qdev.tp2026.gr21.jeuQuizz;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! :)");
     }
 }
