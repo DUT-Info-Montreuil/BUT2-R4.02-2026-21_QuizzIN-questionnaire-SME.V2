444b44c062735ed2f58de67e18db364d5bccdbe2
nourchouari <novrchouari@gmail.com>
2026-05-26 16:22:09 +0200

    initialisation

---
+++ src/main/java/universite_paris8/iut/qdev/tp2026/gr21/jeuQuizz/App.java
@@ -0,0 +1,10 @@
+package universite_paris8.iut.qdev.tp2026.gr21.jeuQuizz;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/universite_paris8/iut/qdev/tp2026/gr21/jeuQuizz/AppTest.java
@@ -0,0 +1,19 @@
+package universite_paris8.iut.qdev.tp2026.gr21.jeuQuizz;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
